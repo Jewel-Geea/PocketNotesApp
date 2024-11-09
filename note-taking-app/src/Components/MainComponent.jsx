24cbe3a402492bbae4ab407ad28a205d592bc153
Jewel Geea George <jewelgeeageorge@gmail.com>
2024-11-09 17:40:35 +0530

    refactoring code

--- note-taking-app/src/Components/MainComponent.jsx
+++ note-taking-app/src/Components/MainComponent.jsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 const MainComponent = () => {
   //on progress
   const [state, setState] = useState(true);
+  
   const toggleChatVisibility = () => {
     setState(!state);
   };
